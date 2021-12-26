63e4bad1d434b772b38ad1bdc3f37c19560edfa1
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2021-12-26 19:54:52 +0300

    Предварительная версия FileService.cs, AppService.cs

--- DarkNotes/services/FileService.cs
+++ DarkNotes/services/FileService.cs
@@ -9,6 +9,9 @@ namespace DarkNotes
         private String _currentFilename;
         private SaveFileDialog _saveFileDialog;
         private OpenFileDialog _openFileDialog;
+
+        private String _currentText = "";
+
         //private FileDialog _fileDialog;
         public FileService(string currentFilename, SaveFileDialog saveFileDialog, OpenFileDialog openFileDialog)
         {
@@ -17,8 +20,24 @@ namespace DarkNotes
             _openFileDialog = openFileDialog;
         }
 
+        public string CurrentText
+        {
+            get => _currentText;
+            set => _currentText = value;
+        }
+
         private void InvertColors(Color prevColor, Color newColor)
         {
+            // richTextBox1.SelectAll();
+            // richTextBox1.ForeColor = left;
+            // richTextBox1.SelectionIndent = -richTextBox1.SelectionHangingIndent;
+            // richTextBox1.SelectionRightIndent = 0;
+            //
+            // richTextBox1.SaveFile(_currentFilename);
+            //
+            // richTextBox1.ForeColor = right;
+            // SetIndents();
+            // richTextBox1.DeselectAll();
         }
 
         private void Save()
@@ -58,12 +77,47 @@ namespace DarkNotes
             }
         }
 
-        public void OpenFile()
+        public void OpenFile(RichTextBox richTextBox1)
         {
+            if (!"".Equals(_currentFilename) || !"".Equals(_currentText) || richTextBox1.TextLength > 0)
+            {
+                DialogResult result = MessageBox.Show("Do you want to save all of your valuable writing?",
+                    "Opening new file", MessageBoxButtons.YesNoCancel);
+                if (result == DialogResult.Yes)
+                {
+                    this.SaveFile();
+                }
+                else if (result == DialogResult.Cancel)
+                    return;
+            }
+
+            if (_openFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            richTextBox1.ResetForeColor();
+            _currentFilename = _openFileDialog.FileName;
+            richTextBox1.LoadFile(_currentFilename);
+            richTextBox1.ForeColor = Color.White;
+            //ToDo: посмотреть, как правильно делать инстансы
+            new AppearanceService().SetIndents();
         }
 
-        public void NewFile()
+        public void NewFile(RichTextBox richTextBox1)
         {
+            if (richTextBox1.TextLength > 0)
+            {
+                DialogResult result = MessageBox.Show("Do you want to save all of your valuable writing?",
+                    "Closing the app", MessageBoxButtons.YesNoCancel);
+                if (result == DialogResult.Yes)
+                {
+                    this.SaveFile();
+                }
+                else if (result == DialogResult.Cancel)
+                    return;
+            }
+
+            richTextBox1.Clear();
+            // reset to default
         }
     }
 }
